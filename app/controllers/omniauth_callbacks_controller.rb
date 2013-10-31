e1142b99d1694f85bbb9241dd585aba77ea6ede7
Jean-Baptiste Goulain <jibai31@gmail.com>
2013-10-31 17:53:30 +0100

    Add Facebook and Twitter authentication.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,9 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
+
+  def create_and_sign_user
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      flash.notice = "Signed in Through Google!"
+      flash.notice = "Bienvenue sur PolitiQA !"
       sign_in_and_redirect user
     else
       # Should never get there
@@ -11,5 +12,17 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash.notice = "You are almost Done! Please provide a password to finish setting up your account"
       redirect_to new_user_registration_url
     end
+  end
+
+  def google_oauth2
+    create_and_sign_user
+  end
+
+  def facebook
+    create_and_sign_user
+  end
+
+  def twitter
+    create_and_sign_user
   end
 end
